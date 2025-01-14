358f48a9d476018f9b0ecee630d956a04acad085
Sandikha24 <144902581+Sandikha24@users.noreply.github.com>
2025-01-14 20:16:09 +0800

    Update header.php

--- header.php
+++ header.php
@@ -3,17 +3,17 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title><?php echo $_SESSION['system']['name'] ?></title>
-        <!-- Favicon-->
+      
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
+     
         <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
+
         <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-        <!-- Third party plugin CSS-->
+        
         <link href="admin/assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
+        
         <link href="admin/assets/vendor/bootstrap-datepicker/css/bootstrap-datepicker.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
         <link type="text/css" rel="stylesheet" href="admin/assets/css/jquery-te-1.4.0.css">
