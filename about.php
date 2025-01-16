ed96c711d2f69ed05980873447a193c222301dc2
Kevinananda13 <kvnanandaid@gmail.com>
2025-01-17 00:44:58 +0800

    ubah about

--- about.php
+++ about.php
@@ -1,7 +1,13 @@
-    <section class="page-section">
-        <div class="container">
-            <h2 class="text-center">About Us</h2>
-    <?php echo html_entity_decode($_SESSION['system']['about_content']) ?>        
-            
-        </div>
-        </section>
+<section class="page-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="gambar_tim.jpg" alt="Tim Kami" class="img-fluid">
+      </div>
+      <div class="col-md-6">
+        <h2 class="text-center">About Us</h2>
+        <?php echo html_entity_decode($_SESSION['system']['about_content']) ?>
+      </div>
+    </div>
+  </div>
+</section>   
