227f21a27c011709af155f7c9e7247f65b8d2e5d
Kevinananda13 <kvnanandaid@gmail.com>
2025-01-17 13:29:17 +0800

    progress header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<header class="header">
+
+   <div class="flex">
+
+      <a href="#" class="logo">foodies</a>
+
+      <nav class="navbar">
+         <a href="admin.php">add products</a>
+         <a href="products.php">view products</a>
+      </nav>
+
+      <a href="cart.php"cart <span>0</span> </a>
+
+   </div>
+
+</header>
