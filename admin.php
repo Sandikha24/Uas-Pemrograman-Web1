abd872e2d38822f1c26eb54c2647dcf831d528da
Sandikha24 <144902581+Sandikha24@users.noreply.github.com>
2025-01-19 12:13:57 +0800

    nambahin dkit admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,14 @@
+<?php
+@include 'config.php';
+
+
+if (isset($_['add_product'])){
+   $p_name = $_POST['p_name'];
+   $p_price = $_POST['p_price'];
+   $p_image = $_FILES['p_image']['name'];
+   $p_image_tmp_name = $_FILES['p_image']['tmp_name'];
+   $p_image_folder = 'upload_img';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
